18e858afaa3ecb754c8ada6d9b3bfc2d304ffd82
renardein <drleo2001@gmail.com>
2022-10-07 00:45:59 +0700

    add webserver

---
+++ modules/webserver/index.js
@@ -0,0 +1,11 @@
+
+const http = require("http");
+ 
+http.createServer(function(request, response){
+     
+    response.setHeader("UserId", 12);
+    response.setHeader("Content-Type", "text/html; charset=utf-8;");
+    api.getMe().then((me) => { response.write(`<h2>@${me.username} bot is running...</h2>`); });
+    response.end();
+}).listen(8080);
+module.exports.run = run;
