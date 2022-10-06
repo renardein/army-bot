12cfea7d4e43c2ba48d46b920c5884445647749e
renardein <drleo2001@gmail.com>
2022-10-07 00:55:54 +0700

    fix webserver

--- modules/webserver/index.js
+++ modules/webserver/index.js
@@ -1,11 +1,13 @@
 
 const http = require("http");
- 
-http.createServer(function(request, response){
+ function run(api){
+    http.createServer(function(request, response){
      
     response.setHeader("UserId", 12);
     response.setHeader("Content-Type", "text/html; charset=utf-8;");
     api.getMe().then((me) => { response.write(`<h2>@${me.username} bot is running...</h2>`); });
     response.end();
 }).listen(8080);
+ }
+
 module.exports.run = run;
