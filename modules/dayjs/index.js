2d90d75d8eb520aef73ca810953b94d0b4ece041
renardein <drleo2001@gmail.com>
2022-10-10 22:05:16 +0700

    modified:   index.js 	modified:   modules/bot/answers.json 	modified:   modules/bot/index.js 	new file:   modules/dayjs/index.js 	modified:   modules/filesystem/index.js 	deleted:    modules/servetimer/index.js 	modified:   modules/subscribers/index.js 	new file:   modules/webserver/index,js

---
+++ modules/dayjs/index.js
@@ -0,0 +1,45 @@
+const dayjs = require('dayjs'),
+    fsm = require('../filesystem');
+customParseFormat = require('dayjs/plugin/customParseFormat'),
+    utc = require('dayjs/plugin/utc'),
+    timezone = require('dayjs/plugin/timezone') // dependent on utc plugin
+dayjs.extend(customParseFormat);
+dayjs.extend(timezone);
+dayjs.extend(utc);
+dayjs.tz.setDefault("Asia/Krasnoyarsk")
+const datetime = "2014-06-01 12:00";
+const tz = "America/New_York"
+function getServeTime(startTime, type) {
+
+    const startDate = dayjs(startTime, "DD-MM-YYYY", 'ru', true),
+        endDate = dayjs(startTime, "DD-MM-YYYY", 'ru', true).add(1, 'year');
+
+    switch (type) {
+        case 'command': {
+            let data = {
+                startDate: startDate.format('DD-MM-YYYY'), //Дата начала
+                endDate: endDate.format('DD-MM-YYYY'),//Дата конца
+                totalDays: endDate.diff(startDate, 'day'),  // Дней всего
+                daysPassed: dayjs().diff(startDate, 'day'),  // Дней прошло
+                daysLeft: endDate.diff(dayjs(), 'day'), //Дней осталось
+                progress: Math.round(dayjs().diff(startDate, 'day') / endDate.diff(startDate, 'day') * 100), //Прогресс в процентах
+                subscribers: fsm.readJsonFile('database/subscribers.json').length //Количество подписчиков
+            }
+
+            return data;
+
+        }
+        case 'mailer': {
+            let data = {
+                daysPassed: dayjs().diff(startDate, 'day'),  // Дней прошло
+                daysLeft: endDate.diff(dayjs(), 'day'), //Дней осталось
+                progress: Math.round(dayjs().diff(startDate, 'day') / endDate.diff(startDate, 'day') * 100), //Прогресс в процентах
+            }
+            return data;
+        }
+    }
+
+    return data
+}
+
+module.exports = { getServeTime };
--- modules/subscribers/index.js
+++ modules/subscribers/index.js
@@ -1,6 +1,7 @@
 var filesystem = require('../filesystem'),
-dataPath= './database/subscribers.json';
+    dataPath = './database/subscribers.json';
 
+//Проверка наличия chatId в subscribers.json
 function isExists(chatId) {
     var json = filesystem.readJsonFile(dataPath)
     if (json.includes(chatId))
@@ -8,16 +9,19 @@ function isExists(chatId) {
     else
         return false
 }
+//Добавление нового chatId в subscribers.json
 function add(chatId) {
     var json = filesystem.readJsonFile(dataPath);
     json.push(chatId);
-    filesystem.writeJsonFile(dataPath,json);
+    filesystem.writeJsonFile(dataPath, json);
 }
+//Удаление chatId в subscribers.json
 function remove(chatId) {
     var json = filesystem.readJsonFile(dataPath)
     var filteredJson = json.filter(x => x !== chatId);
     filesystem.writeJsonFile(dataPath, filteredJson);
 }
+//Подсчет количества подписчиков в subscribers.json
 function count(chatId) {
     var json = filesystem.readJsonFile(dataPath)
     return json.length;
@@ -25,7 +29,4 @@ function count(chatId) {
 
 
 
-module.exports.isExists = isExists;
-module.exports.add = add;
-module.exports.remove = remove;
-module.exports.count = count;
+module.exports = { isExists, add, remove, count }
