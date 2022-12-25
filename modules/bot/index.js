17eaf9214f5bd2897e9d4d0981de01209996242d
renardein <drleo2001@gmail.com>
2022-12-25 23:51:56 +0700

    Fixed language strings

--- modules/bot/index.js
+++ modules/bot/index.js
@@ -2,8 +2,9 @@ const subscriber = require('../../database/models/subscribers'),
     fs = require('fs'),
     answers = require('./gen_answer.js'),
     timer = require('./timer'),
-    dateRegEx = /(\d{2}[./-]\d{2}[./-]\d{4}$)/ig,
-    timeRegEx = /^(2[0-3]|[01]?[0-9]):([0-5]?[0-9])$/;
+    dateRegEx = /^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[012])-\d{4}$/,
+    timeRegEx = /^(2[0-3]|[01]?[0-9]):([0-5]?[0-9])$/,
+    configFile = require('../../config');
 
 async function run(bot, config) {
     bot.on('text', async msg => {
@@ -11,76 +12,91 @@ async function run(bot, config) {
         switch (commandData[0]) {
             case '/start': {
                 await bot.sendAudio(msg.chat.id, 'CQACAgIAAxkBAANqYzxS_P4wP09n5lf8b68i_gQH38UAArIdAAJ2tehJbllN1BK0CtkqBA', {
-                    caption: answers.staticCommands[0].answer
+                    caption: answers.start
                 });
                 break;
             }
+
             case '/about': {
-                await bot.sendMessage(msg.chat.id, answers.staticCommands[1].answer, { parse_mode: 'markdown', disable_web_page_preview: true });
+                await bot.sendMessage(msg.chat.id, answers.about, { parse_mode: 'markdown', disable_web_page_preview: true });
                 break;
             }
+
             case '/subscribe': {
                 try {
                     // Проверка статуса подписки
                     if (!await subscriber.isExists(msg.chat.id)) {
                         //Если нет, то подписываем
                         await subscriber.add(msg.chat.id);
-                        await bot.sendMessage(msg.chat.id, answers.events[1].answer);
+                        await bot.sendMessage(msg.chat.id, answers.userSubscribed);
                     } else {
-                        await bot.sendMessage(msg.chat.id, answers.events[2].answer);
+                        await bot.sendMessage(msg.chat.id, answers.userExists);
                     }
                 } catch (err) {
                     console.log('\x1b[41m', `[ERROR] ${err.message}`);
                 }
                 break;
             }
+
             case '/unsubscribe': {
                 try {
                     // Проверка статуса подписки
                     if (await subscriber.isExists(msg.chat.id)) {
                         //Если есть, то отписываем
                         await subscriber.remove(msg.chat.id);
-                        await bot.sendMessage(msg.chat.id, answers.events[3].answer);
+                        await bot.sendMessage(msg.chat.id, answers.userUnsubscribed);
                     } else {
-                        await bot.sendMessage(msg.chat.id, answers.events[4].answer);
+                        await bot.sendMessage(msg.chat.id, answers.userNotSubscribed);
                     }
                 } catch (err) {
                     console.log('\x1b[41m', `[ERROR] ${err.message}`);
                 }
                 break;
             }
-            case '/debug': {
-                const uptime = Math.round(process.uptime());
-                const usedMem = Math.round(process.memoryUsage().rss / 1024 / 1024);
-                const message = answers.getTemplateString(answers.dynamicCommands[1].answer, ['%uptime%', '%usedMem%'], [uptime, usedMem]);
-                await bot.sendMessage(msg.chat.id, message, { parse_mode: 'markdown' });
-                break;
-            }
+
             case '/army': {
                 let data = await timer.getServeTime(config.chosenDate, 'command');
                 await bot.sendMessage(msg.chat.id,
-                    answers.getTemplateString(answers.dynamicCommands[0].answer,
+                    answers.getTemplateString(answers.army,
                         ['%startDate%', '%endDate%', '%totalDays%', '%daysPassed%', '%daysLeft%', '%subscribers%', '%progressGraphical%'],
                         [data.startDate, data.endDate, data.totalDays, data.daysPassed, data.daysLeft, data.subscribers, data.progressGraphical]),
                     { parse_mode: 'markdown' })
                 break;
             }
+
+            case '/debug': {
+                const uptime = Math.round(process.uptime());
+                const usedMem = Math.round(process.memoryUsage().rss / 1024 / 1024);
+                const message = answers.getTemplateString(answers.debug, ['%uptime%', '%usedMem%'], [uptime, usedMem]);
+                await bot.sendMessage(msg.chat.id, message, { parse_mode: 'markdown' });
+                break;
+            }
+
             case '/set': {
                 // Проверяем, является ли отправитель сообщения администратором
                 if (msg.from.id === config.adminUserId) {
                     switch (commandData[1]) {
                         case 'date': {
-                            // Проверяем, соответствует ли введенная дата регулярному выражению
                             if (dateRegEx.test(commandData[2])) {
-                                config.chosenDate = commandData[2];
                                 // Записываем изменения в файл конфигурации
-                                fs.writeFileSync("../config.json", JSON.stringify(config), error => console.log(error));
-                                await bot.sendMessage(msg.chat.id, answers.getTemplateString(answers.events[5].answer, ['%startingPoint%'], [commandData[2]]));
+                                try {
+                                    configFile.setDate(commandData[2]);
+                                    await bot.sendMessage(
+                                        msg.chat.id,
+                                        `Начальная дата успешно установлена: ${commandData[2]}`,
+                                    );
+                                } catch (error) {
+                                    console.error(error);
+                                }
                             } else {
-                                await bot.sendMessage(msg.chat.id, answers.getTemplateString(answers.events[6].answer, ['%startingPoint%'], [commandData[2]]));
+                                await bot.sendMessage(
+                                    msg.chat.id,
+                                    `Неверный формат даты: ${commandData[2]}`,
+                                );
                             }
                             break;
                         }
+
                         case 'mailer': {
                             switch (commandData[2]) {
                                 case 'cron': {
@@ -116,7 +132,7 @@ async function run(bot, config) {
                 // Добавить чат в список подписчиков
                 await subscriber.add(msg.chat.id);
                 // Отправить приветствие в чат
-                await bot.sendMessage(msg.chat.id, answers.getTemplateString(answers.events[7].answer, ['%chatName%'], [msg.chat.title]));
+                await bot.sendMessage(msg.chat.id, answers.getTemplateString(answers.groupChatAdded, ['%chatName%'], [msg.chat.title]));
             }
         } else {
             // В противном случае, удалить чат из списка подписчиков
