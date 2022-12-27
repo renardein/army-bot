81d468dabbd1e9226ef040788e544ac1bc2a8986
renardein <drleo2001@gmail.com>
2022-12-27 22:20:38 +0700

    fix db path

--- database/dbAdapter.js
+++ database/dbAdapter.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const sqlite3 = require('sqlite3').verbose();
-const dbFile = './database/database.db';
+
 
 // Проверяем, существует ли файл базы данных
 if (!fs.existsSync(dbFile)) {
     // Создаем файл базы данных
-    fs.closeSync(fs.openSync(dbFile, 'w'));
+    fs.closeSync(fs.openSync('./database/database.db', 'w'));
 }
-const db = new sqlite3.Database(dbFile, sqlite3.OPEN_READWRITE, (err) => {
+const db = new sqlite3.Database('./database/database.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
         console.error('\x1b[34m', `[Database] ${err.message}`);
         process.exit(0)
